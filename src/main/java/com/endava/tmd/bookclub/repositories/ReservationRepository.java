b9d5d7d140f5fd13e925050b3008dacade812585
andreeajurca98 <106539079+andreeajurca98@users.noreply.github.com>
2022-07-11 08:34:47 +0300

    Delete src directory

--- src/main/java/com/endava/tmd/bookclub/repositories/ReservationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.endava.tmd.bookclub.repositories;
-
-import com.endava.tmd.bookclub.entity.Reservation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-}
